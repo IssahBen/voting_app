5142ec995df10786fd52aa98acecc95d6ce06275
IssahBen <benedictissah@gmail.com>
2024-03-12 18:49:30 -0500

    initial commit

---
+++ db/migrate/20240312234548_create_candidates.rb
@@ -0,0 +1,9 @@
+class CreateCandidates < ActiveRecord::Migration[7.1]
+  def change
+    create_table :candidates do |t|
+      t.string :first_name 
+      t.string :last_name
+      t.timestamps
+    end
+  end
+end
